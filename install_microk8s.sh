a0568b634ec05368b1b8621c653676e7c9742c56
manulsan <alex.damosys@gmail.com>
2021-12-27 13:13:42 +0900

    install_microk8s.sh changed

--- install_microk8s.sh
+++ install_microk8s.sh
@@ -1,11 +1,12 @@
 // reference : https://microk8s.io/docs/clustering
-sudo snap install microk8s --classic --channel=1.23/stable
+// https://blog.naver.com/manulsan/222604948960
+sudo snap install microk8s --classic --channel=latest/stable
 sudo ufw allow in on cni0 && sudo ufw allow out on cni0
 sudo ufw default allow routed
-sudo microk8s enable dns dashboard storage
+sudo microk8s enable dns dashboard storage ingress
 sudo microk8s kubectl get all --all-namespaces
-token=$(microk8s kubectl -n kube-system get secret | grep default-token | cut -d " " -f1)
-sudo microk8s kubectl -n kube-system describe secret $token
+#token=$(microk8s kubectl -n kube-system get secret | grep default-token | cut -d " " -f1)
+#sudo microk8s kubectl -n kube-system describe secret $token
 #sudo microk8s kubectl create deployment microbot --image=dontrebootme/microbot:v1
 #sudo microk8s kubectl scale deployment microbot --replicas=2
 #sudo microk8s kubectl expose deployment microbot --type=NodePort --port=80 --name=microbot-service
